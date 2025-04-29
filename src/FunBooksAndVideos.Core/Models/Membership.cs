324e4f3f44a2df917d69b57be4848e375aa02151
ananthat <ananthat@gmail.com>
2025-04-29 23:44:55 +0100

    Update Membership.cs
    Added price attributes

--- src/FunBooksAndVideos.Core/Models/Membership.cs
+++ src/FunBooksAndVideos.Core/Models/Membership.cs
@@ -4,4 +4,5 @@ namespace FunBooksAndVideos.Core.Models;
 public class Membership : Item
 {
     public MembershipType Type { get; init; }    // IsPhysical remains false (default from Item)
+    public decimal Price { get; init; }
 }
